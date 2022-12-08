962fc25af1c85e985ddb28ce3cf9eb9e0022c494
Your Name <you@example.com>
2022-12-08 17:19:12 +0000

    DELETE

--- csharp/src/Products.cs
+++ csharp/src/Products.cs
@@ -16,7 +16,7 @@ namespace Ecommerce.Products
         }
 
         [Function("Products")]
-        public ProductBindings Run([HttpTrigger(AuthorizationLevel.Anonymous, "get", "post")] 
+        public ProductBindings Run([HttpTrigger(AuthorizationLevel.Anonymous, "get", "post", "put", "delete")] 
         HttpRequestData req, ProductDTO product)
         {
             _logger.LogInformation("C# HTTP trigger function processed a request.");
@@ -27,8 +27,8 @@ namespace Ecommerce.Products
             response.WriteString("Welcome to Azure Functions!");
 
             if(req.Method == "GET") {  }    // get
-            if(req.Method == "POST") { return Create(req, product); }   // create
-            if(req.Method == "PUT") {  }    // update
+            if(req.Method == "POST") { return Create(req, product); }
+            if(req.Method == "PUT") {  return Update(req, product); }
             if(req.Method == "DELETE") {  } // delete
 
             return new ProductBindings
@@ -36,6 +36,30 @@ namespace Ecommerce.Products
                 HttpResponse = response,
             };
         }
+
+        private static ProductBindings Update(HttpRequestData req, ProductDTO product)
+        {
+            var response = req.CreateResponse(HttpStatusCode.OK);
+            response.Headers.Add("Content-Type", "text/plain; charset=utf-8");
+
+            if (string.IsNullOrEmpty(product.Id))
+            {
+                response.StatusCode = HttpStatusCode.BadRequest;
+                response.WriteString("Product ID is required");
+                var resp = new ProductBindings { HttpResponse = response };
+
+                return resp;
+            }
+
+            response.WriteString($"Record {product.Id} updated");
+
+            return new ProductBindings
+            {
+                HttpResponse = response,
+                Product = product
+            };
+        }
+
 
         private static ProductBindings Create(HttpRequestData req, ProductDTO product)
         {
